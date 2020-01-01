bba71d3d1478bbd199a0f5ae56da5f5013ede131
Panagiotis Ntagkinis <panagiotisntagkinis@gmail.com>
2020-01-01 18:38:28 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,14 +3,18 @@
 board = [" "," "," "," "," "," "," "," "," "]
 
 def position_taken?(board, play)
-#  if board[play] == " " || "" || nil
-#     return false
+  if board[play] != " " || "" || nil
+     if board[play] == "X" || "O"
+        return true
+     end
+  else
+     return false
 #  elsif board[play] == "X" || "O"
 #    return true
-  if board[play] == "X" || "O"
-     return true
-  end
-  if board[play] == " " || "" || nil
-    return false
+# if board[play] == "X" || "O"
+#     return true
+#  end
+#  if board[play] == " " || "" || nil
+#    return false
   end
 end
